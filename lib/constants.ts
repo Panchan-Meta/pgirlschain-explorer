bb302666f4fe4a0271fa49c7cbc1311cea7e27af
Panchan <121082403+Panchan-Meta@users.noreply.github.com>
2025-08-19 18:26:18 +0900

    Use shared token metadata object

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,8 @@
-export const DECIMALS = 8;
+export const DECIMALS = 10;
 export const SYMBOL = "PGIRLS";
+
+// Consolidated token metadata for convenience when importing as a group
+export const TOKEN = {
+  DECIMALS,
+  SYMBOL,
+} as const;
